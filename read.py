ffc5b11508880b493693f88a58d28f4b4eba3de3
Eli <yuchien.elliot@gmail.com>
2022-04-08 16:24:22 -0700

    calc avg length

--- read.py
+++ read.py
@@ -6,8 +6,14 @@ with open('reviews.txt','r') as f:
 		count += 1 # count = count + 1
 		if count % 10000 == 0:
 			print(len(data)) # find the length(count of items) of list
-print(len(data))
+print('loading complete, total ', len(data), ' rows of record')
 
 # print(data[0])
 # print('----------------')
 # print(data[1])
+
+sum_len = 0
+for d in data:
+	sum_len = sum_len + len(d)
+
+print('On average the length of comment is ', sum_len/len(data))
