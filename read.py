8bd984f11938fdbe51e8d333674b2b02b907dfa2
Eli <yuchien.elliot@gmail.com>
2022-05-19 23:58:25 -0700

    Word Count Done

--- read.py
+++ read.py
@@ -4,17 +4,14 @@ with open('reviews.txt','r') as f:
 	for line in f:
 		data.append(line)
 		count += 1 # count = count + 1
-		if count % 10000 == 0:
+		if count % 100000 == 0:
 			print(len(data)) # find the length(count of items) of list
-print('loading complete, total ', len(data), ' rows of record')
+print('Loading complete, total ', len(data), ' rows of record')
 
-# print(data[0])
-# print('----------------')
-# print(data[1])
-
+# List
 sum_len = 0
 for d in data:   # Set d as the length of each comment
-	sum_len = sum_len + len(d)
+	sum_len = sum_len + len(d)#
 
 print('On average the length of comment is ', sum_len/len(data))
 
@@ -23,22 +20,102 @@ for d in data:
 	if len(d) < 100:
 		new.append(d)
 print(len(new), ' comments in total less than 100 characters')
-print(new[0])
+print(new[0])#
 
 
-# good = []
+good = []
+for d in data:
+	if 'good' in d:
+		good.append(d)
+print(len(good), ' comments in total mentioned good')
+
+#List comprehension
+#good = [d for d in data if 'good' in d]
+#print(good)#
+
+#bad = ['bad' in d for d in data]
+#print(bad)
+
+# Original version
+# bad = []
 # for d in data:
-# 	if 'good' in d:
-# 		good.append(d)
-# print(len(good), ' comments in total mentioned good')
+# 	bad.append('bad' in d)
+
+
+
+# Count word
+
+
+wc = {} # word_count
+for d in data:
+	words = d.split(' ')
+	for word in words:
+		if word in wc:
+			wc[word] += 1
+		else:
+			wc[word] = 1 # add new key into dict
+
+for word in wc:
+	sorted_d = dict (sorted(wc.items(), key=lambda item: item[1],reverse=True))
+	if wc[word] > 1000000:
+		print(word, wc[word])
+
+print(len(wc))
+print(wc['Allen'])
+
+while True:
+	word = input('Which word do you want to look up?')
+	if word == 'q':
+		break
+	if word in wc:
+		print(word, 'appears ', wc[word], ' times')
+	else: 
+		print('This word does not exist.')
+		
+print('Thanks for using this app!')
+
+
+
+
+
+
+
+
+
+
+
+
+# print(data[0])
+# print('----------------')
+# print(data[1])
+
+sum_len = 0
+for d in data:   # Set d as the length of each comment
+	sum_len = sum_len + len(d)#
+
+print('On average the length of comment is ', sum_len/len(data))
+
+new = []
+for d in data:
+	if len(d) < 100:
+		new.append(d)
+print(len(new), ' comments in total less than 100 characters')
+print(new[0])#
+
+
+good = []
+for d in data:
+	if 'good' in d:
+		good.append(d)
+print(len(good), ' comments in total mentioned good')
 
 
 #List comprehension
-good = [1 for d in data if 'good' in d]
-print(good)
+#good = [d for d in data if 'good' in d]
+#print(good)#
 
-bad = ['bad' in d for d in data]
-print(bad)
+#bad = ['bad' in d for d in data]
+#print(bad)
 
 # Original version
 # bad = []
